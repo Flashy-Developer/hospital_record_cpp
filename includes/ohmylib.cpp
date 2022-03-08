cecc815362578bad82aa95065fd74928bc33915e
PKGzGMTH <peakungg@gmail.com>
2022-03-08 21:13:03 +0700

    Initial commit

---
+++ includes/ohmylib.cpp
@@ -0,0 +1,11 @@
+#ifndef OHMYLIB_CPP
+# define OHMYLIB_CPP
+# include <iostream>
+
+/*	define here	*/
+
+using namespace std;
+
+/*	Function here	*/
+
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include "../includes/ohmylib.cpp"
+
+int main(void)
+{
+	cout << "Hello from cpp\n";
+	return (0);
+}
